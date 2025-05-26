dc8e7beac9cdbb87f56652c686a462418933140b
Cabangbang <raphacabangbang@gmail.com>
2025-05-26 19:48:47 +0100

    more practice

--- test1.py
+++ test1.py
@@ -1,2 +1,16 @@
 name = input("Enter your name: ")
-print ("Your name is: " + name)
+age = int(input("Enter your age: "))
+
+if age < 18:
+  print(name + ", is a minor.")
+else:
+  print(name + ", is an adult.")
+
+#check if age is odd or even
+if age % 2 == 0:
+  print(name + ", your age is even.")
+elif age % 2 != 0:
+  print(name + ", your age is odd.")
+else:
+  print(  "Invalid age")
+  
